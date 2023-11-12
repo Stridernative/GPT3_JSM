f8b73924f3112e52039266792a8254d7c743e996
StriderNative <GabeCreaCS@gmail.com>
2023-11-12 14:30:14 -0800

    Header JSX and CSS completed
    fixed a bug in CSS forgot to add flex-direction row

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -10,21 +10,20 @@ const Header = () => {
         <h1 className="gradient__text"> Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
         <p> Yet bed any for travelling assitance indulgence unpleasing. Not thoughts all exercise blessing. INdulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
-         <div className="gpt3__header-content__input">
+        <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your Email Address" />
           <button type="button">Get Started </button>
-         </div>
+        </div>
 
-         <div className="gpt3__header-content__people">
+        <div className="gpt3__header-content__people">
           <img src={people} />
           <p>1,600 people requested access a visit in last 24 hours</p>
-         </div>
-      </div>
-
-      <div className="gpt3__header-image">
-        <img src={ai} />
         </div>
       </div>
+      <div className="gpt3__header-image">
+        <img src={ai} />
+      </div>
+    </div>
   )
 };
 
